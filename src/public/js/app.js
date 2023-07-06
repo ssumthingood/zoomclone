d68046d31f8f7d757dc3efcd8c3f6f74a4224d8f
ssumthingood <ssumthingood@gmail.com>
2023-07-06 17:55:56 +0900

    0706 socket message with server & browser

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,17 @@
-alert("hi!");
-hello;
+const socket = new WebSocket(`ws://${window.location.host}`);
+socket.addEventListener("open",()=>{
+    console.log("Connected to Browser");
+})
+
+socket.addEventListener("message",(message)=>{
+    console.log("Just got ",message.data," from the server");
+    console.log("msg", message);
+})
+
+socket.addEventListener("close",()=>{
+    console.log("Disconnected from Server");
+});
+
+setTimeout(()=>{
+    socket.send("hello from browser");
+},5000);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
+import http from "http";
 import express from "express";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -9,5 +11,25 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 console.log("hello");
-const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+// const handleListen = () => console.log("Listening on http://localhost:3000");
+const handleListen = () => console.log("Listening on http://localhost:3000, ws");
+
+// app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+// const handleConnection=(socket)=>{
+//     console.log(socket)
+// }
+
+wss.on("connection", (socket)=>{
+    console.log("Connected to server");
+    socket.on("close",()=>{console.log("Disconnected from Browser")});
+    socket.on("message",(message)=>{
+        console.log(message);
+    })
+    socket.send("hello?");
+})
+
+server.listen(3000,handleListen);
